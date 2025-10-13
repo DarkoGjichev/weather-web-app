e3f79ef155afcb276f3211767fb56d9b400b5056
DarkoGjichev <darkogicev111@gmail.com>
2025-10-13 06:41:10 +0100

    add calendar component to main with default css and minimum date set to 2022 following data availability from API

--- src/components/weather/WeatherOverview.jsx
+++ src/components/weather/WeatherOverview.jsx
@@ -2,10 +2,14 @@ import { useEffect, useState } from "react";
 import getWeather from "../../api/getWeather";
 import WeatherDay from "./WeatherDay";
 import WeatherDaily from "./WeatherDaily";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 function WeatherOverview({ latitude, longitude }) {
   const [weatherData, setWeatherData] = useState(null);
   const [date, setDate] = useState(null);
+  const minDate = new Date(2022, 0, 1);
+  const maxDate = new Date();
 
   useEffect(() => {
     getWeather(latitude, longitude).then((data) => {
@@ -18,6 +22,7 @@ function WeatherOverview({ latitude, longitude }) {
 
   return (
     <main>
+      <Calendar value={date} minDate={minDate} maxDate={maxDate} />
       <WeatherDay
         current={weatherData.current}
         currentUnits={weatherData.current_units}
