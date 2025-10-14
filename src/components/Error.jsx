046c6ee7099bd291d4ad3698623c56182ca4df07
DarkoGjichev <darkogicev111@gmail.com>
2025-10-14 21:13:28 +0100

    add loading and error components

---
+++ src/components/Error.jsx
@@ -0,0 +1,10 @@
+function Error() {
+  return (
+    <main>
+      <h2>404</h2>
+      <p>Oops! Something went wrong.</p>
+    </main>
+  );
+}
+
+export default Error;
---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+function Loading() {
+  return (
+    <main>
+      <h2>Loading...</h2>
+    </main>
+  );
+}
+
+export default Loading;
