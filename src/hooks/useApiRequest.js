99fb7d10439566a73a219f255a433b721f9f48c4
DarkoGjichev <darkogicev111@gmail.com>
2025-10-16 00:47:00 +0100

    add custom hook for dynamic api calls

---
+++ src/hooks/useApiRequest.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+
+function useApiRequest(apiFunction, ...arg) {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    apiFunction(...arg)
+      .then((data) => {
+        console.log(data, "<<<<<<<<<<<<<<<<");
+        setData(data);
+      })
+      .catch((error) => {
+        setError(error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [...arg, apiFunction]);
+
+  return { isLoading, error, data, setData };
+}
+
+export default useApiRequest;
