debc81de5a92c9c897e540cfab979e70805152cb
DarkoGjichev <darkogicev111@gmail.com>
2025-10-15 20:52:00 +0100

    add conditional logic for calendar component rendering

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -1,28 +1,38 @@
 import Calendar from "react-calendar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCalendarDays } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 function DatePicker({ date, setDate }) {
+  const [active, setActive] = useState(false);
   const minDate = new Date(2022, 0, 1);
   const maxDate = new Date();
+
   const handleChange = (date) => {
     date.setHours(12, 0, 0, 0);
     const formattedDate = date.toISOString().split("T")[0];
     setDate(formattedDate);
+    setActive(false);
   };
+
   return (
     <main className="relative max-w-[1035px] w-[100%] mx-auto">
-      <button className="flex flex-row items-center ml-auto text-white">
+      <button
+        onClick={() => setActive(!active)}
+        className="flex flex-row items-center ml-auto text-white"
+      >
         <h3>{date}</h3>
         <FontAwesomeIcon icon={faCalendarDays} />
       </button>
-      <Calendar
-        className="absolute right-0 z-1"
-        value={date}
-        onChange={handleChange}
-        minDate={minDate}
-        maxDate={maxDate}
-      />
+      {active && (
+        <Calendar
+          className="absolute right-0 z-1"
+          value={date}
+          onChange={handleChange}
+          minDate={minDate}
+          maxDate={maxDate}
+        />
+      )}
     </main>
   );
 }
