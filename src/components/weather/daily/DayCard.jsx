6ed5c7c6c229f9509cf540c7a3391b5c03a00738
DarkoGjichev <darkogicev111@gmail.com>
2025-10-15 20:02:26 +0100

    fix - small style changes

--- src/components/weather/daily/DayCard.jsx
+++ src/components/weather/daily/DayCard.jsx
@@ -14,9 +14,9 @@ function DayCard({ daily, dailyUnits }) {
   return sevenDayForecast.map((day) => (
     <div
       key={day.time}
-      className="flex flex-row gap-2 text-center bg-white/20 rounded-[20px] p-2"
+      className="flex flex-row gap-4 text-center items-center bg-white/20 rounded-[20px] p-2"
     >
-      <p>
+      <p className="w-[50px]">
         {new Date(day.time).toLocaleDateString([], {
           weekday: "short",
           month: "short",
@@ -27,13 +27,13 @@ function DayCard({ daily, dailyUnits }) {
         {day.temperature_min}
         {dailyUnits.temperature_2m_min}
       </p>
-      {" - "}
+      {"-"}
       <p>
         {day.temperature_max}
         {dailyUnits.temperature_2m_max}
       </p>
       <p>{weatherIcon(day.weather_code)}</p>
-      <p className="w-[70px] ml-auto">
+      <p className="w-[80px] ml-auto">
         <FontAwesomeIcon icon={faDroplet} className="text-xs" />{" "}
         {day.precipitation}
         {dailyUnits.precipitation_sum}
