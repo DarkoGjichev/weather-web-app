ce49b1e24eae2d8ed4bf8942ce8a4be4af306b48
DarkoGjichev <darkogicev111@gmail.com>
2025-10-16 10:36:45 +0100

    add custom hook while fetching weather data

--- src/components/weather/WeatherOverview.jsx
+++ src/components/weather/WeatherOverview.jsx
@@ -1,50 +1,52 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import getWeather from "../../api/getWeather";
 import getHistoricalWeather from "../../api/getHistoricalWeather";
 import WeatherDay from "./WeatherDay";
 import WeatherDaily from "./WeatherDaily";
 import DatePicker from "../DatePicker";
 import "react-calendar/dist/Calendar.css";
+import useApiRequest from "../../hooks/useApiRequest";
+import Loading from "../Loading";
+import Error from "../Error";
 
 function WeatherOverview({ latitude, longitude }) {
-  const [weatherData, setWeatherData] = useState(null);
   const today = new Date().toISOString().split("T")[0];
   const [date, setDate] = useState(today);
 
-  useEffect(() => {
-    if (!date || date === today) {
-      getWeather(latitude, longitude).then((data) => {
-        setWeatherData(data);
-        setDate(data.current.time.split("T")[0]);
-      });
-    } else {
-      getHistoricalWeather(latitude, longitude, date).then((data) => {
-        setWeatherData(data);
-      });
-    }
-  }, [date, latitude, longitude]);
-
-  if (!weatherData) return <p>Loading...</p>;
-
-  return (
-    <>
-      <DatePicker date={date} setDate={setDate} />
-      <main className="max-w-[1035px] w-full flex flex-col lg:flex-row gap-4 mx-auto">
-        <WeatherDay
-          current={weatherData.current}
-          currentUnits={weatherData.current_units}
-          hourly={weatherData.hourly}
-          date={date}
-          daily={weatherData.daily}
-          dailyUnits={weatherData.daily_units}
-        />
-        <WeatherDaily
-          daily={weatherData.daily}
-          dailyUnits={weatherData.daily_units}
-        />
-      </main>
-    </>
+  const {
+    data: weatherData,
+    error,
+    isLoading,
+  } = useApiRequest(
+    date === today ? getWeather : getHistoricalWeather,
+    latitude,
+    longitude,
+    date
   );
+
+  if (isLoading) return <Loading />;
+  if (error) return <Error />;
+  if (weatherData) {
+    return (
+      <>
+        <DatePicker date={date} setDate={setDate} />
+        <main className="max-w-[1035px] w-full flex flex-col lg:flex-row gap-4 mx-auto">
+          <WeatherDay
+            current={weatherData.current}
+            currentUnits={weatherData.current_units}
+            hourly={weatherData.hourly}
+            date={date}
+            daily={weatherData.daily}
+            dailyUnits={weatherData.daily_units}
+          />
+          <WeatherDaily
+            daily={weatherData.daily}
+            dailyUnits={weatherData.daily_units}
+          />
+        </main>
+      </>
+    );
+  }
 }
 
 export default WeatherOverview;
