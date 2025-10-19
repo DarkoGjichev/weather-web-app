5d17ba2bbf32ea4e70f0d06e91ed6b8b466c26a5
DarkoGjichev <darkogicev111@gmail.com>
2025-10-19 16:11:56 +0100

    add search for city component

---
+++ src/components/header/Search.jsx
@@ -0,0 +1,9 @@
+function Search() {
+  return (
+    <form>
+      <input type="search" id="city" placeholder="Search for a city" />
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+export default Search;
